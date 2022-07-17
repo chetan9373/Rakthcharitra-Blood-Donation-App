154a8b31784251ed533947d48272c5cc56c1f088
radhika2096 <sanghairadhika@gmail.com>
2022-07-17 22:40:02 +0530

    radhika

---
+++ app/src/main/java/com/harsh1310/rakkktcharitr/operationmodel.java
@@ -0,0 +1,15 @@
+package com.harsh1310.rakkktcharitr;
+
+public class operationmodel {
+    private String operation;
+
+
+        public operationmodel(String countryName) {
+          operation= countryName;
+
+        }
+
+        public String getOperation() {
+            return operation;
+        }
+}
