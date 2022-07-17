73ca5453bcb71a656d7ae2a1d74a8e243018f76c
harshal1310 <hygosavi9834@gmail.com>
2022-07-17 16:40:06 -0400

    uptodate

---
+++ app/src/main/java/com/harsh1310/rakkktcharitr/opmodel.java
@@ -0,0 +1,14 @@
+package com.harsh1310.rakkktcharitr;
+
+public class opmodel {
+    String operation;
+    public opmodel(String countryName) {
+        operation= countryName;
+
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+}
